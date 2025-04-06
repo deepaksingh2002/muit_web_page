a11cc86227d8571e793271e638a1f3e60aa4ec8a
Deepak Singh <deepakksingh1202@gmail.com>
2025-04-07 00:11:36 +0530

    router

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./NavBar/Navbar";
+import Hero from "./Hero/Hero";
+import Campus from "./Campus/Campus";
+import Footer from "./Footer/Footer";
+import Programs from "./Programs/Programs";
+import Tastemonial from "./Tastimonial/Tastemonial";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+
+export{
+    Navbar,
+    Hero,
+    Footer,
+    Campus,
+    Programs,
+    Tastemonial,
+    About,
+    Contact
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,35 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import {BrowserRouter} from "react-router"
+import { Navbar, Programs, About, Campus,Contact } from './components/index.js';
+import {createBrowserRouter, RouterProvider} from "react-router"
+import { StrictMode } from 'react';
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children:[
+      {
+        path:"/",
+        element:<Navbar/>
+      },{
+        path:'/program',
+        element:<Programs />
+      },{
+        path:'/campus',
+        element:<Campus />
+      },{
+        path:'/contact',
+        element:<Contact />
+      }
+    ]
+
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/muit_web_page'>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
