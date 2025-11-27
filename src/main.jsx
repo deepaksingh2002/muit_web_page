e30d0cfd4c599a8ec888f8681984b8b784d2ce19
Deepak Singh <deepakksingh1202@gmail.com>
2025-11-27 21:37:58 +0530

    fix some common error in index and main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,11 @@
-//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import { Navbar, Programs, About, Campus,Contact } from './components/index.js';
-import {createBrowserRouter, RouterProvider} from "react-router"
 import { StrictMode } from 'react';
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App />,
-    children:[
-      {
-        path:"/",
-        element:<Navbar/>
-      },{
-        path:'/program',
-        element:<Programs />
-      },{
-        path:'/campus',
-        element:<Campus />
-      },{
-        path:'/contact',
-        element:<Contact />
-      }
-    ]
-
-  }
-])
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
